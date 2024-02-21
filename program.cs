40a4a2b2faea8b92e1e566e667c94ddfc4f38268
2.Account <151151449+FinnAller@users.noreply.github.com>
2024-02-21 13:34:01 +0100

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,5 +1,24 @@
 using Items;
 using System;
 class Program{
-  
+  static List<Items> cart = new List<Items>;
+  static void Main(string[] args){
+      Console.Title = "Shopping cart";
+      cart.Clear();
+      while(true){
+          Console.Clear();
+      }
+  }
+  static void Shopping(){
+    Console.Clear();
+    while(true){
+      Console.Clear();
+      Console.WriteLine("Cola - 1.99$");
+      Console.WriteLine("Auto - 20.000$");
+      Console.WriteLine("Deutschlandticket - 50$");
+      Console.WriteLine("Kopfhörer - 90$");
+      Console.Write("Select Item!");
+    }
+  }
+
 }
